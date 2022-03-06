17b07cad3dce086c86966fd184eb209bdbc7ec1e
mauriciojeanfl <93798414+mauriciojeanfl@users.noreply.github.com>
2022-03-06 16:20:38 -0300

    Final

--- EDA - Iris Dataset.R
+++ EDA - Iris Dataset.R
@@ -37,10 +37,77 @@ summary(data)
 #density plot
 #pie graph
 
-#Income - Discreto ou contínuo? - contínuo
+#INCOME - Discreto ou contínuo? - contínuo
 summary(data$Income)
 boxplot(data$Income)
 
 #spread
 hist(data$Income)
-plot(density(data$Income))
+plot(density(data$Income), main = "Income density spread")
+
+#EDUCATION
+summary(data$Education)
+plot(data$Education)
+data %>% ggplot() + geom_bar(mapping = aes(x = Education), fill = "blue")
+
+#MARITAL STATUS
+summary(data$Marital.Status)
+plot(data$Marital.Status)
+pie(table(data$Marital.Status))
+
+#Change to Factor
+
+data$Children = factor(data$Children)
+summary(data$Children)
+plot(data$Children)
+
+
+bought = data %>% filter(Purchased.Bike == "Yes")
+
+plot(bought$Children)
+
+plot(data$Commute.Distance)
+head(data)
+
+#Análise multivariada
+
+#CATEGORICA E CONTINUA
+
+by(data$Income, data$Education, summary)
+by(data$Income, data$Education, mean)
+by(data$Income, data$Education, median)
+
+#plotando ggplot entre education and income.
+data %>% ggplot() + geom_boxplot(mapping = aes(x = Education, y = Income, colour = Education))
+
+#plotando density plot entre education e Income
+data %>% ggplot() + geom_density(mapping = aes(x = Income, fill = Education))
+
+#Comprou ou não comprou a moto em relação ao nível de educação.
+xtabs(~Education+Purchased.Bike, data)
+plot(xtabs(~Education+Purchased.Bike, data))
+
+#Comprou ou não comprou a moto em relação a profissão
+xtabs(~Occupation+Purchased.Bike, data)
+plot(xtabs(~Occupation+Purchased.Bike, data))
+
+install.packages("gmodels")
+library(gmodels)
+
+#TABELA DE CHI² PARA VERIFICAR SE O NÍVEL DE EDUCAÇÃO TME OU NÃO RELAÇÃO NA COMPRA DA MOTO
+CrossTable(data$Education, data$Purchased.Bike, chisq = T, prop.t = F)
+
+#Se o valor de p<= 0.05 é estatisticamente significante.Neste caso deu 0.00004.. o que significa que o nível de educação é significante em relação a compra da moto ou não.
+
+names(data)
+#TABELA DE CHI? PARA VERIFICAR SE A SITUAÇÃO MARITAL TEM INTERFERÊNCIA NA COMPRA DA MOTO
+CrossTable(data$Marital.Status, data$Purchased.Bike, chisq = T, prop.t = F)
+#Neste caso temos um p valor de 0.005, o que significa que a situação marital tem inteferência na compra ou não de moto.
+
+
+#Continuos e continuos
+
+scatter.smooth(data$Age, data$Income)
+
+#verifica-se como o income se comporta ao longo das idades.
+
